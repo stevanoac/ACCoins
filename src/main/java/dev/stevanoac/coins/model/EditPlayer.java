17809831be40753e108e42f02f8ea67043e3b696
steven <stevanoclaros@gmail.com>
2023-08-27 20:41:36 -0400

    Init

--- src/main/java/dev/stevanoac/coins/model/EditPlayer.java
+++ src/main/java/dev/stevanoac/coins/model/EditPlayer.java
@@ -5,30 +5,31 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 public class EditPlayer {
-    private int coins;
-    public EditPlayer(Player player) {
-        if (Coins.getPlayerDocument(player) == null) coins = 0;
-        this.coins = Coins.getPlayerDocument(player).getInteger("coins");
+    private int coinsAmount;
+    private Coins coins;
+    public EditPlayer(Coins coins, Player player) {
+        if (coins.getPlayerDocument(player) == null) coinsAmount = 0;
+        this.coinsAmount = coins.getPlayerDocument(player).getInteger("coins");
     }
 
     public EditPlayer(OfflinePlayer player) {
-        if (Coins.getOfflinePlayerDocument(player) == null) coins = 0;
-        this.coins = Coins.getOfflinePlayerDocument(player).getInteger("coins");
+        if (coins.getOfflinePlayerDocument(player) == null) coinsAmount = 0;
+        this.coinsAmount = coins.getOfflinePlayerDocument(player).getInteger("coins");
     }
 
     public int getCoins() {
-        return coins;
+        return coinsAmount;
     }
 
     public void setCoins(int coins) {
-        this.coins = coins;
+        this.coinsAmount = coins;
     }
 
     public void addCoins(int coins) {
-        this.coins = this.coins + coins;
+        this.coinsAmount = this.coinsAmount + coins;
     }
 
     public void revokeCoins(int coins) {
-        this.coins = this.coins - coins;
+        this.coinsAmount = this.coinsAmount - coins;
     }
 }
